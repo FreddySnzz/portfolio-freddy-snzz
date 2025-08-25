2b61dc927a033888974cc77e1d08fd3755f3a351
FreddySnzz <fredsonsantos@seduc.pi.gov.br>
2025-08-25 18:51:53 -0300

    feat: added responsive layout

--- src/components/layout/ApresentationContainer.tsx
+++ src/components/layout/ApresentationContainer.tsx
@@ -7,39 +7,38 @@ export default function ApresentationContainer() {
   // animação de digitação no ola? com outras frases após terminar de escrever?
 
   return (
-  <section id="home" className="flex flex-col h-screen w-full scroll-hidden">
-    <div className="flex grow items-center justify-center mt-6 bg-amber-500 group scroll-hidden">
-      <div className="flex flex-col items-center justify-center h-2/3 w-full bg-red-400 transition-all duration-500 group-hover:w-3/4">
-        <h1 className="text-4xl text-center font-bold mb-4">
-          Olá! Bem-vindo(a) ao meu Portfolio! :)
-        </h1>
-        <p className="text-sm mb-8 text-center italic animate-pulse">
-          Bom te ver por aqui.
-        </p>
-      </div>
-      <div className="flex flex-col grow h-2/3 w-1/4 hover:w-full transition-all duration-500 scroll-hidden">
-        <div
-          ref={scrollRef}
-          className="h-full overflow-x-auto scroll-hidden scroll-smooth-touch whitespace-nowrap select-none"
-        >
-          <div className="inline-block w-10/12 h-full bg-red-500 rounded-full">1</div>
-          <div className="inline-block w-10/12 h-full bg-blue-400 rounded-full">2</div>
-          <div className="inline-block w-10/12 h-full bg-green-400 rounded-full">3</div>
-          <div className="inline-block w-10/12 h-full bg-yellow-400 rounded-full">4</div>
-          <div className="inline-block w-10/12 h-full bg-purple-400 rounded-full">5</div>
-          <div className="inline-block w-10/12 h-full bg-purple-500 rounded-full">6</div>
-          <div className="inline-block w-10/12 h-full bg-purple-600 rounded-full">7</div>
-          <div className="inline-block w-10/12 h-full bg-purple-700 rounded-full">8</div>
-          <div className="inline-block w-10/12 h-full bg-purple-800 rounded-full">9</div>
-          <div className="inline-block w-10/12 h-full bg-purple-900 rounded-full">10</div>
+    <section id="home" className="flex flex-col h-screen w-full scroll-hidden">
+      <div className="md:flex grow items-center justify-center mt-6 group scroll-hidden bg-amber-500 ">
+        <div className="flex flex-col items-center justify-center h-1/2 md:h-2/3 w-full transition-all duration-500 md:group-hover:w-3/4 bg-red-400 ">
+          <h1 className="text-4xl text-center font-bold mb-4">
+            Olá! Bem-vindo(a) ao meu Portfolio! :)
+          </h1>
+          <p className="text-sm mb-8 text-center italic animate-pulse">
+            Bom te ver por aqui.
+          </p>
+        </div>
+        <div className="flex flex-col grow h-1/2 md:h-2/3 md:w-1/4 hover:w-full transition-all duration-500 scroll-hidden bg-white">
+          <div
+            ref={scrollRef}
+            className="h-full overflow-x-auto scroll-hidden scroll-smooth-touch whitespace-nowrap select-none"
+          >
+            <div className="inline-block w-full md:w-10/12 h-full bg-red-500 rounded-full">1</div>
+            <div className="inline-block w-full md:w-10/12 h-full bg-blue-400 rounded-full">2</div>
+            <div className="inline-block w-full md:w-10/12 h-full bg-green-400 rounded-full">3</div>
+            <div className="inline-block w-full md:w-10/12 h-full bg-yellow-400 rounded-full">4</div>
+            <div className="inline-block w-full md:w-10/12 h-full bg-purple-400 rounded-full">5</div>
+            <div className="inline-block w-full md:w-10/12 h-full bg-purple-500 rounded-full">6</div>
+            <div className="inline-block w-full md:w-10/12 h-full bg-purple-600 rounded-full">7</div>
+            <div className="inline-block w-full md:w-10/12 h-full bg-purple-700 rounded-full">8</div>
+            <div className="inline-block w-full md:w-10/12 h-full bg-purple-800 rounded-full">9</div>
+            <div className="inline-block w-full md:w-10/12 h-full bg-purple-900 rounded-full">10</div>
+          </div>
         </div>
       </div>
-    </div>
 
-    {/* <div className="flex justify-center items-center bg-green-400">
-      <ButtonOverflowRight />
-    </div> */}
-  </section>
-);
-
+      {/* <div className="flex justify-center items-center bg-green-400">
+        <ButtonOverflowRight />
+      </div> */}
+    </section>
+  );
 }
