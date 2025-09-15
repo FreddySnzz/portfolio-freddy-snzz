0aebae91ce6a30a32ed5098a4e79fe37788dfaa2
FreddySnzz <fredsonsantos@seduc.pi.gov.br>
2025-09-15 11:13:24 -0300

    feat: added button link to github repository in project card

--- src/components/buttons/ButtonRepositoryGithub.tsx
+++ src/components/buttons/ButtonRepositoryGithub.tsx
@@ -6,14 +6,14 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 
 export default function ButtonRepositoryUrl({ repositoryUrl }: ButtonProps) {
   return (
-    <div>
+    <div className="flex items-center">
       <a
         href={repositoryUrl}
         target="_blank"
         rel="noopener noreferrer"
       >
         <button 
-          className={`p-2 cursor-pointer 
+          className={`cursor-pointer 
             text-gray-400 hover:text-gray-600 dark:text-gray-700 dark:hover:text-gray-400`}
         >
           <FaExternalLinkAlt className="text-2xl" />
